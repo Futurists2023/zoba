0d6e9075c80b3072d88b2c27005e5443300ab794
MankweAI <kgoshimankwe@outlook.com>
2026-03-16 21:30:40 +0200

    Added Google GSC tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: "zoba",
   description:
     "zoba is a mobile-first cost of living planner for young professionals, couples, and families.",
+  verification: {
+    google: "Uwjqmg3-739dulHr8Vl_2HuS2abol3-Gs43Eaw6edH0",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
